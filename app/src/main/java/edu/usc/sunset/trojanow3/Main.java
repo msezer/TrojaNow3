77531027d10ce3ed35eb3183c3fb95ce9e4419c4
msezer <msezer2009@gmail.com>
2015-04-29 20:06:41 -0700

    mainpage scroll

--- app/src/main/java/edu/usc/sunset/trojanow3/Main.java
+++ app/src/main/java/edu/usc/sunset/trojanow3/Main.java
@@ -1,23 +1,52 @@
 package edu.usc.sunset.trojanow3;
 
 import android.content.Context;
-import android.location.LocationListener;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 import android.content.Intent;
+import android.view.TextureView;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Main extends ActionBarActivity {
+
+    public static TextView tweetText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tweetText = (TextView) findViewById(R.id.main_tweet_textview);
+        tweetText.setMovementMethod(new ScrollingMovementMethod());
+
+        String dummy_tweets = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit." +
+        "Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis" +
+        "parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu," +
+        "pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet" +
+        "nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo." +
+        "Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus" +
+        "elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu," +
+        "consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a," +
+        "tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet." +
+        "Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui." +
+        "Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero," +
+        "sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar," +
+        "hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut" +
+        "libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus" +
+        "tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna." +
+        "Sed consequat, leo eget bibendum sodales, augue velit cursus nunc, quis";
+
+        tweetText.setText(dummy_tweets);
+
+
     }
 
     // Link to the compose activity
